d3d32cafd9f8c34d48246c66859accadd8f4f534
PodM <m.a.podbolotov@gmail.com>
2021-02-05 00:14:41 +0300

    hw_5

--- UI_Vkontakte/FriendsViewController.swift
+++ UI_Vkontakte/FriendsViewController.swift
@@ -1,14 +1,17 @@
 //
-//  FriendsTableViewController.swift
+//  FriendsViewController.swift
 //  UI_Vkontakte
 //
-//  Created by Михаил Подболотов on 01.01.2021.
+//  Created by Михаил Подболотов on 31.01.2021.
 //
 
 import UIKit
 
-class FriendsTableViewController: UITableViewController {
+class FriendsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var characterPicker: CharacterPicker!
+    
     var networkService: NetworkService!
     var friends: [Friend]!
     var sections = [String]()
@@ -17,6 +20,9 @@ class FriendsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         networkService = NetworkService()
         friends = networkService.getFriends()
         
@@ -26,6 +32,8 @@ class FriendsTableViewController: UITableViewController {
             sections.append(String(char))
         }
         sections.sort(by: <)
+        characterPicker.chars = sections
+        characterPicker.setupUi()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -39,18 +47,39 @@ class FriendsTableViewController: UITableViewController {
             }
     }
 
+    @IBAction func characterPicked(_ sender: CharacterPicker) {
+        let selectedChar = characterPicker.selectedChar
+        var indexPath = IndexPath(item: 0, section: 0)
+        for (index, section) in sections.enumerated() {
+            if selectedChar == section {
+                indexPath = IndexPath(item: 0, section: index)
+            }
+        }
+        tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+    }
+    
+    @IBAction func panGestureAction(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: characterPicker)
+        let coef = Int(characterPicker.frame.height) / sections.count
+        
+        let letterIndex = Int(location.y) / coef
+        
+        if letterIndex < sections.count && letterIndex >= 0 {
+            characterPicker.selectedChar = sections[letterIndex]
+        }
+    }
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return sections.count
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section]
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         var tempArr = [Friend]()
         for friend in friends {
@@ -63,7 +92,7 @@ class FriendsTableViewController: UITableViewController {
 //        return friends.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FriendsTableViewCell {
             var tempArr = [Friend]()
             for friend in friends {
@@ -97,7 +126,7 @@ class FriendsTableViewController: UITableViewController {
         return UITableViewCell()
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
         var tempArr = [Friend]()
         for friend in friends {
